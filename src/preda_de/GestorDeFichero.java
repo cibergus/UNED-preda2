e58e805202ac1aa24faddc9b3921e48a65de8b26
CiberGus <gusleer@gmail.com>
2022-06-30 09:00:25 +0200

    Update GestorDeFichero.java

--- src/preda_de/GestorDeFichero.java
+++ src/preda_de/GestorDeFichero.java
@@ -1,7 +1,5 @@
 /*
-  @author	Augusto Javier Ibañez Garcia
- * 	 email: aibanez122@alumno.uned.es
- * 	 DNI:   25.404.287M
+  @author	cibergus
  * @version 2.0 => 12 Octubre 2018  ///  1.0 => 7 Octubre 2018
  */
 
